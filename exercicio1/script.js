9a156bc83d398e054390fce0263c8faba2135065
kanyfreitas <karolinyfreitas99@gmail.com>
2022-08-03 18:37:29 -0300

    a11

---
+++ exercicio1/script.js
@@ -0,0 +1,18 @@
+const sacolao = [];
+sacolao.push({
+    nome: 'morango',
+    preco: 5,
+    disponibilidade: true
+},
+{
+    nome: 'uva',
+    preco: 2,
+    disponibilidade: false
+},
+{
+    nome: 'maracuja',
+    preco: 3.50,
+    disponibilidade: true
+})
+
+console.log(sacolao)
